4ba686be44915124d46ebfd945dd69492d0b1f5e
Andrew Vasin <45873049+mindrusher@users.noreply.github.com>
2026-02-12 12:48:18 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,17 +1,1 @@
-# -*- coding: utf-8 -*-
-# Конфигурация подключения к базе данных
-
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "mysql+pymysql://root:root@db:3306/orgs_db"
-)
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
-
-class Base(DeclarativeBase):
-    pass
+from db.session import Base, SessionLocal, engine
